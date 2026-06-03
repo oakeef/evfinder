a2e455927b7711d2393dba60d5e79a36f763e08e
Evan <oakeef@gmail.com>
2026-06-03 18:40:34 -0300

    trim vehicle data from csv to fit Vehicle type

--- src/services/evApi.ts
+++ src/services/evApi.ts
@@ -4,14 +4,33 @@ import type { Vehicle } from '../types/vehicle';
 const GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT7IkL6sNkpOuQIMnJ3KqPuwqvKLIQNJyqCE4PzfAYoEzA3yMEx2jRp1VxY4QjEDPUToQtQGtzDeuDb/pub?gid=1313510290&single=true&output=csv';
 
 
-export const getEvData = () => {
+export const getEvData = (): Promise<Vehicle[]> => {
   return new Promise((resolve, reject) => {
-    Papa.parse<Vehicle>(GOOGLE_SHEET_URL, {
+    Papa.parse<any>(GOOGLE_SHEET_URL, {
       download: true,
       header: true,
       skipEmptyLines: true,
       complete: (results) => {
-        resolve(results.data); 
+        const trimmedVehicles: Vehicle[] = results.data.map((row: any, index: number) => {
+            return {
+                id: index,
+                year: row.year,
+                brand: row.brand,
+                model: row.model,
+                lowestPrice: row.lowestPrice,
+                rangeKm: row.rangeKm,
+                batteryCapacityKwh: row.batteryCapacityKwh,
+                bodyType: row.bodyType,
+                storage: row.storage,
+                seats: row.seats,
+                drivetrain: row.drivetrain,
+                horsepower: row.horsepower,
+                torque: row.torque,
+                zeroToSixty: row.zeroToSixty,
+                weightKg: row.weightKg,
+            };
+        });
+        resolve(trimmedVehicles); 
       },
       error: (error) => {
         reject(error);
